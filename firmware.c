e0c68ae6f294bbdd528c60d22410ca156ce89712
Richard Hughes <richard@hughsie.com>
2012-05-16 18:39:41 +0100

    Release test firmware 1.1.7

--- firmware.c
+++ firmware.c
@@ -117,7 +117,7 @@ void CHugLowPriorityISRPlaceholder (void)
 }
 
 /* ensure this is incremented on each released build */
-static UINT16	FirmwareVersion[3] = { 1, 1, 6 };
+static UINT16	FirmwareVersion[3] = { 1, 1, 7 };
 
 #pragma udata
 
